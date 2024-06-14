bc67cdf8e958bae83c4c7cc3649f9237f161a1ad
Abhijit Aher <seabhi.aher@gmail.com>
2024-06-14 15:33:32 +0530

    app components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div>Navbar</div>
+        <div>Recent Data</div>
+        <div>
+          Inventory Chart
+        </div>
+        <div>Avg MSRP Chart</div>
+        <div>History Log</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
